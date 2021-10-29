b22f83affe6107622085ef79651a452d4751c74b
MarcusSten <malmeloon@gmail.com>
2021-10-29 09:08:36 +0300

    add lesson4

--- index.js
+++ index.js
@@ -1,36 +1,66 @@
-const fs = require ("fs");
+const fs = require ("fs/promises");
+const {lstatSync} =require ("fs");
+const path = require("path");
+const {join} = require("path");
+const inquirer = require("inquirer");
+const yargs = require("yargs");
 
-const readStream = fs.createReadStream("./access.log", "utf8");
-const ip_1 = /89.123.1.41/;
-const ip_2 = /34.48.240.111/;
-const file_1 = './89.123.1.41_requests.log';
-const file_2 = './34.48.240.111_requests.log';
+const options = yargs
+  .positional ("d", {
+    describe: "path to dir",
+    default: process.cwd()
+  })
+  .positional ("p", {
+    describe: "pattern",
+    default: ""
+  })
+  .argv;
 
-const stream1 = fs.createWriteStream(file_1, {flags: "a", encoding: "utf8"});
-const stream2 = fs.createWriteStream(file_2, {flags: "a",  encoding: "utf8"});
+let currentDir = options.d;
 
-let readed = 0;
-var line = 0;
-fs.stat('./access.log', (err,stats) => line = stats.size/50);
-let counter = 1;
-readStream.on("data", (chunk) => {
-  let res = chunk.toString().split("\n");
+const isFile = async (path) => (await fs.lstat(path)).isFile();
 
-  res.forEach((item) => {
-    if (ip_1.test(item)) {
-      stream1.write(item + "\n");
+class ListItem {
+  constructor (path, filename) {
+     this.path = path;
+     this.filename = filename;
+  }
+  get isDir () {
+    return lstatSync(this.path).isDirectory();
+  }
+}
+const run = async () => {
+  const list = await fs.readdir(currentDir);
+  const items = list.map (filename =>  new ListItem(join(currentDir, filename), filename));
+
+const item = await  inquirer
+    .prompt([{
+            name: "fileName",
+            type: "list",
+            message: `Choose file: ${currentDir}`,
+            choices: items.map(item =>({name: item.filename, value: item})),
+        }])
+    .then(answer => answer.fileName);
+
+    if(item.isDir) {
+      currentDir = item.path;
+      return await run()
+    } else {
+      const data = await fs.readFile(item.path, "utf-8");
+      if (options.p === null) {
+        console.log(data);
+      } else {
+        const lines = data.split("\n");
+        lines.forEach(line => {
+          const regex = new RegExp(options.p);
+          if (regex.test(line)) {
+            console.log(line);
+          }
+        });
+      }
+         console.log("No matches");
+        return;
     }
-    if (ip_2.test(item)) {
-     stream2.write(item + "\n");
-    }
-  });
-  readed = readed + 64*1024;
-  if (readed > line*counter) {
-      counter++
-      process.stdout.write(`*`);
-    }
-});
+ }
 
-readStream.on("end", () => {
-  console.log("Streamming done.");
-});
+run();
